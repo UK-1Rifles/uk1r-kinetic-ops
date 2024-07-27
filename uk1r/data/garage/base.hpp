e68ad3a3dbdcc82c471344933c774d81762ca7b5
Luke <lukecoffey88@gmail.com>
2024-07-27 22:54:07 +0100

    Started garage config framework

--- uk1r/data/garage/base.hpp
+++ uk1r/data/garage/base.hpp
@@ -2,9 +2,27 @@ class base {
   requiredAddons[] = {};
   displayName = "BASE";
   scope = 0;
-  cars[] = {};
-  mechanized[] = {};
-  armour[] = {};
-  helicopters[] = {};
-  planes[] = {};
+  class cars {
+    class vehicle_class {
+      class = "";
+      condition = "";
+      class inventory {
+        item_1 = count;
+        item_2 = count;
+        item_3 = count;
+      };
+    };
+  };
+  class mechanised {
+
+  };
+  class armour {
+
+  };
+  class helicopters {
+
+  };
+  class planes {
+
+  };
 };
---
+++ uk1r/data/garage/blufor_vanilla.hpp
@@ -0,0 +1,20 @@
+class blufor_vanilla : base {
+  requiredAddons[] = {};
+  displayName = "BLUFOR (Vanilla)";
+  scope = 2;
+  class cars {
+    
+  };
+  class mechanised {
+
+  };
+  class armour {
+
+  };
+  class helicopters {
+
+  };
+  class planes {
+
+  };
+};
